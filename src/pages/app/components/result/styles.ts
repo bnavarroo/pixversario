e1a12d4e14e4349d1b32550ba63218e68781979a
Bruno Navarro <62071446+bnavarroo@users.noreply.github.com>
2022-07-20 13:29:47 -0300

    fix: ajuste ui do resultado

--- src/pages/app/components/result/styles.ts
+++ src/pages/app/components/result/styles.ts
@@ -3,11 +3,10 @@ import styled from 'styled-components';
 export const Wrapper = styled.div`
   width: 100%;
   font-size: 20px;
+  min-height: 34px;
 `;
 
 export const Text = styled.p`
-  min-height: 34px;
-
   b {
     font-size: 22px;
   }
